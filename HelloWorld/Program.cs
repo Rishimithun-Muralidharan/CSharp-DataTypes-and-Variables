dc27937b5ae1c5bb78e54ec22d1d366ee955b7e6
Rishimithun Muralidaran <rishimithun.muralidaran@inivossl.com>
2023-05-08 15:01:03 +0530

    accessed the lower case method of the string class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,33 +1,15 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace HelloWorld 
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            double a = 1.666;
-            double b = 1.888;
-            double abdivide = a / b;
-            
-            
-            Console.WriteLine(a + " divided by " + b + " is " + abdivide);
-
-            float x = 1.666f;
-            float y = 1.888f;
-            float xydivide = x / y;
-
-            Console.WriteLine(x + " divided by " + y + " is " + xydivide);
-
-            long f = 868665675;
-            long g = 675444535354;
-            long fgdivide = g / f;
-
-            Console.WriteLine(f + " divided by " + g + " is " + fgdivide);
-
-            
-            
-            
+            string myname = "MY NAME IS RISHI";
+            string lowermyname = myname.ToLower();
+            Console.WriteLine(lowermyname);
+            Console.ReadLine();
         }
     }
 }
