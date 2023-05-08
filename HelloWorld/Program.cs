15d85eda197c089dd0ac64c93ac36e9aa7435808
RishimithunMuralidar <RishimithunMuralidar@DESKTOP-TSSG74K>
2023-05-08 12:24:18 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.666;
+            double b = 1.888;
+            double abdivide = a / b;
+            
+            
+            Console.WriteLine(abdivide);
+            Console.Read();
+            
+            
+        }
+    }
+}
